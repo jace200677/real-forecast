d828f2aee273c9440ce285910e5a433261ebcfc2
jace200677 <jacefink17@outlook.com>
2026-04-23 17:21:48 -0500

    Update weather1night.py

--- weather1night.py
+++ weather1night.py
@@ -271,7 +271,7 @@ def main():
     # ✅ APPLY OVERRIDE WINDOW
     data = apply_override(data, now_cst)
 
-    render_image(data)
+    render_image(data, now_cst)
     push_to_github()
 
 if __name__ == "__main__":
